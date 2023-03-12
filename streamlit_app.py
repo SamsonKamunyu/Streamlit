8d77a0f93fdc304b1c8ae87c88574aea7ffb5205
SamsonKamunyu <109947291+SamsonKamunyu@users.noreply.github.com>
2023-03-12 16:31:04 +0300

    Updating app

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,41 +8,41 @@
 #else:
 #    st.write('Goodbye')
 
-import numpy as np
-import altair as alt
-import pandas as pd
-import streamlit as st
+#import numpy as np
+#import altair as alt
+#import pandas as pd
+#import streamlit as st
 
-st.header('st.write')
+#st.header('st.write')
 
 #Example 1
 
-st.write('Hello, *World!* :sunglasses:')
+#st.write('Hello, *World!* :sunglasses:')
 
 #Example 2
 
-st.write(1234)
+#st.write(1234)
 
 #Example 3
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
-st.write(df)
+#df = pd.DataFrame({
+ #   'first column': [1, 2, 3, 4],
+  #  'second column': [10, 20, 30, 40] })
+
+#st.write(df)
 
 #Example 4
 
-st.write('Below is a DataFrame:', df, 'Above is a DataFrame.')
+#st.write('Below is a DataFrame:', df, 'Above is a DataFrame.')
 
 #Example 5
 
-df2 = pd.DataFrame(
-    np.random.randn(200, 3),
-    columns = ['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-    x = 'a', y = 'b', sixe = 'c', color = 'c', tooltip = ['a', 'b', 'c'])
-st.write(c)
+#df2 = pd.DataFrame(
+ #   np.random.randn(200, 3),
+  #  columns = ['a', 'b', 'c'])
+#c = alt.Chart(df2).mark_circle().encode(
+ #   x = 'a', y = 'b', sixe = 'c', color = 'c', tooltip = ['a', 'b', 'c'])
+#st.write(c)
 
 import streamlit as st
 
