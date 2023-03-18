39b0eb2e09daceb337fec5d867f45b0be4907e43
SamsonKamunyu <109947291+SamsonKamunyu@users.noreply.github.com>
2023-03-18 14:02:56 +0300

    Adding an Upload Widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,8 @@
  #   x = 'a', y = 'b', sixe = 'c', color = 'c', tooltip = ['a', 'b', 'c'])
 #git st.write(c)
 
-import streamlit as st
+
+#import streamlit as st
 
 #st.header('st.checkbox')
 
@@ -67,7 +68,8 @@ import streamlit as st
 #if tea:
 #     st.write("Devs take coffee!")
 
-import streamlit as st
+
+#import streamlit as st
 #import pandas as pd
 #import pandas_profiling
 #from streamlit_pandas_profiling import st_profile_report
@@ -110,18 +112,37 @@ import streamlit as st
 #textColor="#FFFFFF"
 #font="monospace"
 
-import streamlit as st
 
-st.title('st.secrets')
+#import streamlit as st
+
+#st.title('st.secrets')
 
 # Everything is accessible via the st.secrets dict:
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["I_like"])
+#st.write("DB username:", st.secrets["db_username"])
+#st.write("My cool secrets:", st.secrets["my_cool_secrets"]["I_like"])
 
 # And the root-level secrets are also accessible as environment variables:
 
-import os
-st.write(
-	"Has environment variables been set:",
-	os.environ["db_username"] == st.secrets["db_username"])
+#import os
+#st.write(
+#	"Has environment variables been set:",
+#	os.environ["db_username"] == st.secrets["db_username"])
+
+import streamlit as st
+import pandas as pd
+
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('Upload a CSV file')
+  
