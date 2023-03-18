fc728e5e3696e0b467e166eecb560c98360390af
SamsonKamunyu <109947291+SamsonKamunyu@users.noreply.github.com>
2023-03-18 10:53:59 +0300

    Theme Customization

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,3 +102,10 @@ font="monospace"
 
 number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number from slider widget is:', number)
+
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
