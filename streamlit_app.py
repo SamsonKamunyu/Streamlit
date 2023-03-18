cffe33c17e27750ddc70076cc33cfac0f62f2cd6
SamsonKamunyu <109947291+SamsonKamunyu@users.noreply.github.com>
2023-03-18 10:49:23 +0300

    LaTeX Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,13 +68,21 @@ import streamlit as st
 #     st.write("Devs take coffee!")
 
 import streamlit as st
-import pandas as pd
-import pandas_profiling as pp
-from streamlit_pandas_profiling import st_profile_report
+#import pandas as pd
+#import pandas_profiling
+#from streamlit_pandas_profiling import st_profile_report
 
-st.header('`streamlit_pandas_profiling`')
+#st.header('`streamlit_pandas_profiling`')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+#df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
-pr = df.profile_report()
-st_profile_report(pr)
+#pr = df.profile_report()
+#st_profile_report(pr)
+
+st.header('st.latex')
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
